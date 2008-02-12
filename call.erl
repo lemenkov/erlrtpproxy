dc89ea51e7e11177ef0e94842f90c432bf843106
Peter Lemenkov <lemenkov@gmail.com>
2008-02-12 11:26:06 +0000

    All UDP data now handled by gen_server (simplify)

--- call.erl
+++ call.erl
@@ -10,8 +10,6 @@
 -export([code_change/3]).
 -export([terminate/2]).
 
--export([watcher/1]).
-
 -record(party, {fdfrom=null, origipfrom=null, origportfrom=null, tagfrom=null, mediaidfrom=null,
 		fdto=null,   origipto=null,   origportto=null,   tagto=null,   mediaidto=null}).
 
@@ -25,24 +23,68 @@ init (_Unused) ->
 	process_flag(trap_exit, true),
 	{ok, Name} = inet:gethostname(),
 	{ok, Ip} = inet:getaddr(Name, inet),
-	Pid = spawn (call, watcher, [self()]),
 	io:format ("::: call[~w] thread started.~n", [self()]),
-	{ok, {Pid, Ip, []}}.
+	{ok, {Ip, []}}.
 
-watcher (Parent) ->
-	receive
-		{udp, Fd, Ip, Port, Msg} ->
-			gen_server:cast(Parent, timeout);
-%			gen_server:call(Parent, {rtp, {Fd, Ip, Port, Msg}});
-		_Other ->
-			gen_server:cast(Parent, unhandled)
-	after 300 ->
-		gen_server:cast(Parent, timeout)
-	end,
-	watcher(Parent).
+% new media id possibly
+handle_call({message_u, {OrigIp, OrigPort, FromTag, MediaId}}, _From, {Ip, Parties}) ->
+	% search for already  existed
+	io:format ("::: call[~w] message [U] OrigIp [~s] OrigPort [~s] MediaId [~s].~n", [self(), OrigIp, OrigPort, MediaId]),
+	case lists:keysearch(FromTag, #party.tagfrom, Parties) of
+		{value, Party} ->
+			io:format("::: call[~w] Already exists!~n", [self()]),
+			{ok, {LocalIp, LocalPort}} = inet:sockname(Party#party.fdfrom),
+			Reply = " " ++ integer_to_list(LocalPort) ++ " " ++ inet_parse:ntoa(LocalIp),
+			{reply, Reply, {Ip, Parties}};
+		false ->
+			% open new Fd and attach it
+			io:format("::: call[~w] Create new socket...", [self()]),
+			case gen_udp:open(0, [{ip, Ip}, {active, true}]) of
+				{ok, Fd} ->
+					io:format(" OK~n"),
+					Party = #party{fdfrom=Fd, origipfrom=OrigIp, origportfrom=OrigPort, tagfrom=FromTag, mediaidfrom=MediaId},
+%					Party = #party{fdfrom=Fd, tagfrom=FromTag, mediaidfrom=MediaId},
+					{ok, {LocalIp, LocalPort}} = inet:sockname(Fd),
+					Reply = " " ++ integer_to_list(LocalPort) ++ " " ++ inet_parse:ntoa(LocalIp),
+					io:format("::: call[~w] answer [~s]~n", [self(), Reply]),
+					{reply, Reply, {Ip, lists:append(Parties, [Party])}};
+				{error, Reason} ->
+					io:format(" FAILED [~p]~n", [Reason]),
+					{reply, {error, udp_error}, {Ip, Parties}}
+			end
+	end;
+
+% handle answer
+handle_call({message_l, {OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo}}, _From, {Ip, Parties}) ->
+	io:format ("::: call[~w] message [L] OrigIp [~s] OrigPort [~s] MediaIdFrom [~s] MediaIdTo[~s].~n", [self(), OrigIp, OrigPort, MediaIdFrom, MediaIdTo]),
+	case lists:keysearch(FromTag, #party.tagfrom, Parties) of
+		{value, Party} ->
+			io:format("::: call[~w] Already exists!~n", [self()]),
+			case gen_udp:open(0, [{ip, Ip}, {active, true}]) of
+				{ok, Fd} ->
+					io:format(" OK~n"),
+					NewParty = Party#party{fdto=Fd, origipto=OrigIp, origportto=OrigPort, tagto=ToTag, mediaidto=MediaIdTo},
+%					NewParty = Party#party{fdto=Fd, tagto=ToTag, mediaidto=MediaIdTo},
+					{ok, {LocalIp, LocalPort}} = inet:sockname(Fd),
+					Reply = " " ++ integer_to_list(LocalPort) ++ " " ++ inet_parse:ntoa(LocalIp),
+					io:format("::: call[~w] answer [~s]~n", [self(), Reply]),
+					List1 = lists:delete(Party, Parties),
+					{reply, Reply, {Fd, Ip, lists:append(List1, [NewParty])}};
+				{error, Reason} ->
+					io:format(" FAILED [~p]~n", [Reason]),
+					{reply, {error, udp_error}, {Ip, Parties}}
+			end;
+		false ->
+			% open new Fd and attach it
+			io:format("::: call[~w] ERROR not found~n", [self()]),
+			{reply, {error, not_found}, {Ip, Parties}}
+	end;
+
+handle_call(_Other, _From, State) ->
+	{noreply, State}.
 
 % rtp from some port
-handle_call({rtp, {Fd, Ip, Port, Msg}}, _From, {Pid, MainIp, Parties}) ->
+handle_cast({rtp, {Fd, Ip, Port, Msg}}, {MainIp, Parties}) ->
 %	io:format ("::: call[~w] message rtp.~n", [self()]),
 	case lists:keysearch(Fd, #party.fdfrom, Parties) of
 		{value, Party} ->
@@ -57,9 +99,9 @@ handle_call({rtp, {Fd, Ip, Port, Msg}}, _From, {Pid, MainIp, Parties}) ->
 				Party#party.origipto == null, Party#party.origportto == null ->
 					NewParty = Party#party{origipto=Ip, origportto=Port},
 					List1 = lists:delete(Party, Parties),
-					{reply, ok, {Pid, MainIp, lists:append(List1, [NewParty])}};
+					{norey,{MainIp, lists:append(List1, [NewParty])}};
 				true ->
-					{reply, ok, {Pid, MainIp, Parties}}
+					{noreply, {MainIp, Parties}}
 			end;
 		false ->
 			case lists:keysearch(Fd, #party.fdto, Parties) of
@@ -82,90 +124,43 @@ handle_call({rtp, {Fd, Ip, Port, Msg}}, _From, {Pid, MainIp, Parties}) ->
 %							io:format ("::: call[~w] message 2.1.2 rtp.~n", [self()]),
 							List1 = lists:delete(Party, Parties),
 %							io:format ("::: call[~w] message 2.1.3 rtp.~n", [self()]),
-							{reply, ok, {Pid, MainIp, lists:append(List1, [NewParty])}};
+							{noreply, {MainIp, lists:append(List1, [NewParty])}};
 						true ->
-							{reply, ok, {Pid, MainIp, Parties}}
+							{noreply, {MainIp, Parties}}
 					end;
 				false ->
 					io:format ("::: call[~w] message UNKNOWN rtp.~n", [self()]),
-					{reply, ok, {Pid, MainIp, Parties}}
+					{noreply, {MainIp, Parties}}
 			end
 	end;
-
-% new media id possibly
-handle_call({message_u, {OrigIp, OrigPort, FromTag, MediaId}}, _From, {Pid, Ip, Parties}) ->
-	% search for already  existed
-	io:format ("::: call[~w] message [U] OrigIp [~s] OrigPort [~s] MediaId [~s].~n", [self(), OrigIp, OrigPort, MediaId]),
-	case lists:keysearch(FromTag, #party.tagfrom, Parties) of
-		{value, Party} ->
-			io:format("::: call[~w] Already exists!~n", [self()]),
-			{ok, {LocalIp, LocalPort}} = inet:sockname(Party#party.fdfrom),
-			Reply = " " ++ integer_to_list(LocalPort) ++ " " ++ inet_parse:ntoa(LocalIp),
-			{reply, Reply, {Pid, Ip, Parties}};
-		false ->
-			% open new Fd and attach it
-			io:format("::: call[~w] Create new socket...", [self()]),
-			case gen_udp:open(0, [{ip, Ip}, {active, true}]) of
-				{ok, Fd} ->
-					io:format(" OK~n"),
-					gen_udp:controlling_process(Fd, Pid),
-					Party = #party{fdfrom=Fd, origipfrom=OrigIp, origportfrom=OrigPort, tagfrom=FromTag, mediaidfrom=MediaId},
-%					Party = #party{fdfrom=Fd, tagfrom=FromTag, mediaidfrom=MediaId},
-					{ok, {LocalIp, LocalPort}} = inet:sockname(Fd),
-					Reply = " " ++ integer_to_list(LocalPort) ++ " " ++ inet_parse:ntoa(LocalIp),
-					io:format("::: call[~w] answer [~s]~n", [self(), Reply]),
-					{reply, Reply, {Pid, Ip, lists:append(Parties, [Party])}};
-				{error, Reason} ->
-					io:format(" FAILED [~p]~n", [Reason]),
-					{reply, {error, udp_error}, {Pid, Ip, Parties}}
-			end
-	end;
-
-% handle answer
-handle_call({message_l, {OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo}}, _From, {Pid, Ip, Parties}) ->
-	io:format ("::: call[~w] message [L] OrigIp [~s] OrigPort [~s] MediaIdFrom [~s] MediaIdTo[~s].~n", [self(), OrigIp, OrigPort, MediaIdFrom, MediaIdTo]),
-	case lists:keysearch(FromTag, #party.tagfrom, Parties) of
-		{value, Party} ->
-			io:format("::: call[~w] Already exists!~n", [self()]),
-			case gen_udp:open(0, [{ip, Ip}, {active, true}]) of
-				{ok, Fd} ->
-					io:format(" OK~n"),
-					gen_udp:controlling_process(Fd, Pid),
-					NewParty = Party#party{fdto=Fd, origipto=OrigIp, origportto=OrigPort, tagto=ToTag, mediaidto=MediaIdTo},
-%					NewParty = Party#party{fdto=Fd, tagto=ToTag, mediaidto=MediaIdTo},
-					{ok, {LocalIp, LocalPort}} = inet:sockname(Fd),
-					Reply = " " ++ integer_to_list(LocalPort) ++ " " ++ inet_parse:ntoa(LocalIp),
-					io:format("::: call[~w] answer [~s]~n", [self(), Reply]),
-					List1 = lists:delete(Party, Parties),
-					{reply, Reply, {Fd, Ip, lists:append(List1, [NewParty])}};
-				{error, Reason} ->
-					io:format(" FAILED [~p]~n", [Reason]),
-					{reply, {error, udp_error}, {Pid, Ip, Parties}}
-			end;
-		false ->
-			% open new Fd and attach it
-			io:format("::: call[~w] ERROR not found~n", [self()]),
-			{reply, {error, not_found}, {Pid, Ip, Parties}}
-	end;
-
-handle_call(_Other, _From, State) ->
-	{reply, ok, State}.
 
 % all other messages
-handle_cast(timeout, Parties) ->
-	io:format ("CALL TIMEOUT~n"),
-	exit ({reason, timeout}),
-	{noreply, Parties};
-
 handle_cast(_Request, State) ->
 	{noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
 
-terminate(Reason, {Pid, _MainIp, _Parties}) ->
-	_Reply = gen_server:cast(Pid, {call_terminated, {self(), Reason}}),
+terminate(Reason, {_MainIp, Parties}) ->
+	ok = gen_server:cast({global, rtpproxy}, {call_terminated, {self(), Reason}}),
+	lists:foreach(
+			fun(X) ->
+				if
+					X#party.fdfrom /= null ->
+						gen_udp:close(X#party.fdfrom)
+				end,
+				if
+					X#party.fdto /= null ->
+						gen_udp:close(X#party.fdto)
+				end
+			end,
+			Parties),
 	io:format("::: call[~w] thread terminated due to reason [~w]~n", [self(), Reason]).
+
+% Fd from which message arrived must be equal to Fd from our state
+handle_info({udp, Fd, Ip, Port, Msg}, State) ->
+	gen_server:cast(self(), {rtp, {Fd, Ip, Port, Msg}}),
+	{noreply, State};
 
 handle_info(Info, State) ->
 	io:format("::: call[~w] Info [~w]~n", [self(), Info]),
--- rtpproxy.erl
+++ rtpproxy.erl
@@ -10,8 +10,6 @@
 -export([code_change/3]).
 -export([terminate/2]).
 
--export([watcher/0]).
-
 % description of call thread
 -record(callthread, {pid=null, callid=null}).
 
@@ -27,10 +25,8 @@ init([IpAtom, PortAtom]) when is_atom(IpAtom), is_atom(PortAtom) ->
 	Port = list_to_integer(atom_to_list(PortAtom)),
 	case gen_udp:open(Port, [{ip, Ip}, {active, true}, list]) of
 		{ok, Fd} ->
-			Pid = spawn(rtpproxy, watcher, []),
-			gen_udp:controlling_process(Fd, Pid),
 			io:format("RTPProxy started at ~s:~w~n", [inet_parse:ntoa(Ip), Port]),
-			{ok, {Fd, Pid, []}};
+			{ok, {Fd, []}};
 		{error, Reason} ->
 			io:format("RTPPROXY not started. Reason [~p]~n", Reason),
 			{stop, Reason}
@@ -40,55 +36,19 @@ init(Args) ->
 	io:format ("Some other [~p]", [Args]),
 	{stop, "Wrong data"}.
 
-watcher() ->
-	receive
-		{udp, Fd, Ip, Port, Msg} ->
-			case string:tokens(Msg, " ;") of
-				[UnixPid, "V"] ->
-					io:format("UnixPid [~s], Cmd [V]...", [UnixPid]),
-					MsgOut = UnixPid ++ " 20040107\n",
-					gen_udp:send(Fd, Ip, Port, [MsgOut]),
-					io:format(" OK~n", []);
-				[UnixPid, "VF", Params] ->
-					io:format("UnixPid [~s], Cmd [VF] Params [~s]...", [UnixPid, Params]),
-					MsgOut = UnixPid ++ " 1\n",
-					gen_udp:send(Fd, Ip, Port, [MsgOut]),
-					io:format(" OK~n", []);
-				[UnixPid, "U", CallId, OrigIp, OrigPort, FromTag, MediaId] ->
-					Reply = gen_server:call({global, rtpproxy}, {message_u, CallId, OrigIp, OrigPort, FromTag, MediaId}),
-					MsgOut = UnixPid ++ Reply,
-					gen_udp:send(Fd, Ip, Port, [MsgOut]);
-				[UnixPid, "L", CallId, OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo] ->
-					case gen_server:call({global, rtpproxy}, {message_l, CallId, OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo}) of
-						{ok, Reply} ->
-							MsgOut = UnixPid ++ Reply,
-							gen_udp:send(Fd, Ip, Port, [MsgOut]);
-						{error, _Error} ->
-							io:format ("Critical error!~n")
-					end;
-				_Other ->
-					io:format("Other command [~s]~n", [Msg])
-			end,
-			watcher();
-		{rtpproxy, destroy} ->
-			io:format("Watcher: got destroy.~n"),
-			ok;
-		_Other ->
-			io:format("Unhandled call.~n"),
-			watcher()
-	after 3000 ->
-		gen_server:cast({global, rtpproxy}, {timeout, self()}),
-		watcher()
-	end.
+handle_call(_Message, _From , State) ->
+	{noreply, State}.
 
-handle_call({message_u, CallId, OrigIp, OrigPort, FromTag, MediaId}, _From, {Fd, Pid, CallsList}) ->
+handle_cast({message_u, UnixPid, {Ip, Port}, {CallId, OrigIp, OrigPort, FromTag, MediaId}}, {Fd, CallsList}) ->
 	io:format("Cmd [U] CallId [~s], OrigIp [~s], OrigPort [~s], FromTag [~s] MediaId [~s]~n", [CallId, OrigIp, OrigPort, FromTag, MediaId]),
 	case lists:keysearch(CallId, #callthread.callid, CallsList) of
 		{value, CallInfo} ->
 			%  update existing record
 			io:format("Session exists. Updating existing.~n"),
 			Reply = gen_server:call(CallInfo#callthread.pid, {message_u, {OrigIp, OrigPort, FromTag, MediaId}}),
-			{reply, Reply, {Fd, Pid, CallsList}};
+			MsgOut = UnixPid ++ Reply,
+			gen_udp:send(Fd, Ip, Port, [MsgOut]),
+			{noreply, {Fd, CallsList}};
 		false ->
 			io:format("Session not exists. Creating new:~n"),
 			% create new session
@@ -97,45 +57,73 @@ handle_call({message_u, CallId, OrigIp, OrigPort, FromTag, MediaId}, _From, {Fd,
 					io:format(" OK~n"),
 					NewCallThread = #callthread{pid=CallPid, callid=CallId},
 					Reply = gen_server:call(CallPid, {message_u, {OrigIp, OrigPort, FromTag, MediaId}}),
-					{reply, Reply, {Fd, Pid, lists:append (CallsList, [NewCallThread])}};
+					MsgOut = UnixPid ++ Reply,
+					gen_udp:send(Fd, Ip, Port, [MsgOut]),
+					{noreply, {Fd, lists:append (CallsList, [NewCallThread])}};
 				Other ->
 					io:format ("RTPPROXY: error creating call! [~w]~n", [Other]),
-					{reply, {error, cannot_start}, {Fd, Pid, CallsList}}
+					% FIXME reply to OpenSER with error message
+					{noreply, {Fd, CallsList}}
 			end
 	end;
 
-handle_call({message_l, CallId, OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo}, _From, {Fd, Pid, CallsList}) ->
+handle_cast({message_l, UnixPid, {Ip, Port}, {CallId, OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo}}, {Fd, CallsList}) ->
 	io:format("Cmd [L] CallId [~s], OrigIp [~s], OrigPort [~s], FromTag [~s] MediaIdFrom [~s] ToTag [~s] MediaIdTo [~s]~n",	[CallId, OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo]),
 	case lists:keysearch(CallId, #callthread.callid, CallsList) of
 		{value, CallInfo} ->
 			% update existing session
-			io:format("Session exists. Updating existing.~n"),
+%			io:format("Session exists. Updating existing.~n"),
 			Reply = gen_server:call(CallInfo#callthread.pid, {message_l, {OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo}}),
-			{reply, {ok, Reply}, {Fd, Pid, CallsList}};
+			MsgOut = UnixPid ++ Reply,
+			gen_udp:send(Fd, Ip, Port, [MsgOut]);
 		false ->
-			io:format("Session not exists. Do nothing.~n"),
-			{reply, {error, not_found}, {Fd, Pid, CallsList}}
-	end.
+			io:format("Session not exists. Do nothing.~n")
+	end,
+	{noreply, {Fd, CallsList}};
 
-handle_cast({call_terminated, {Pid, _Reason}}, {Fd, Pid, CallsList}) ->
+handle_cast({call_terminated, {Pid, _Reason}}, {Fd, CallsList}) ->
 	case lists:keysearch(Pid, #callthread.pid, CallsList) of
 		{value, CallThread} ->
-			{noreply, {Fd, Pid, lists:delete(CallThread, CallsList)}};
+			io:format("RTPPROXY call [~p] closed~n", [Pid]),
+			{noreply, {Fd, lists:delete(CallThread, CallsList)}};
 		false ->
-			{noreply, {Fd, Pid, CallsList}}
+			{noreply, {Fd, CallsList}}
 	end;
 
 handle_cast(_Other, State) ->
 	{noreply, State}.
 
-handle_info({'EXIT', Pid, _Reason}, {Fd, Pid, CallsList}) ->
+% Call died (due to timeout)
+handle_info({'EXIT', Pid, _Reason}, {Fd, CallsList}) ->
 	case lists:keysearch(Pid, #callthread.pid, CallsList) of
 		{value, CallThread} ->
 			io:format("RTPPROXY call [~p] closed~n", [Pid]),
-			{noreply, {Fd, Pid, lists:delete(CallThread, CallsList)}};
+			{noreply, {Fd, lists:delete(CallThread, CallsList)}};
 		false ->
-			{noreply, {Fd, Pid, CallsList}}
+			{noreply, {Fd, CallsList}}
 	end;
+
+% Fd from which message arrived must be equal to Fd from our state
+handle_info({udp, Fd, Ip, Port, Msg}, State) ->
+	case string:tokens(Msg, " ;") of
+		[UnixPid, "V"] ->
+			io:format("UnixPid [~s], Cmd [V]...", [UnixPid]),
+			MsgOut = UnixPid ++ " 20040107\n",
+			gen_udp:send(Fd, Ip, Port, [MsgOut]),
+			io:format(" OK~n", []);
+		[UnixPid, "VF", Params] ->
+			io:format("UnixPid [~s], Cmd [VF] Params [~s]...", [UnixPid, Params]),
+			MsgOut = UnixPid ++ " 1\n",
+			gen_udp:send(Fd, Ip, Port, [MsgOut]),
+			io:format(" OK~n", []);
+		[UnixPid, "U", CallId, OrigIp, OrigPort, FromTag, MediaId] ->
+			ok = gen_server:cast({global, rtpproxy}, {message_u, UnixPid, {Ip, Port}, {CallId, OrigIp, OrigPort, FromTag, MediaId}});
+		[UnixPid, "L", CallId, OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo] ->
+			ok = gen_server:cast({global, rtpproxy}, {message_l, UnixPid, {Ip, Port}, {CallId, OrigIp, OrigPort, FromTag, MediaIdFrom, ToTag, MediaIdTo}});
+		_Other ->
+			io:format("Other command [~s]~n", [Msg])
+	end,
+	{noreply, State};
 
 handle_info(Info, State) ->
 	io:format("RTPPROXY got INFO [~p]~n", [Info]),
@@ -144,8 +132,7 @@ handle_info(Info, State) ->
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
 
-terminate(Reason, {Fd, Pid, _CallsList}) ->
+terminate(Reason, {Fd, _CallsList}) ->
 	gen_udp:close(Fd),
-	Pid ! {rtpproxy, destroy},
 	io:format("RTPPROXY terminated due to reason [~w]~n", [Reason]).
 
