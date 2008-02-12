a3a2d32eeb2008146259859801464e005e5ec15f
Peter Lemenkov <lemenkov@gmail.com>
2008-02-12 21:24:24 +0000

    OK. I fed up trying to sync with my working svn, so I decided to commit it again. Initial revision.

---
+++ rtpproxy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#cd /root/rtpproxy && erl +K true -name rtpproxy -noshell -s rtpproxy start_link "127.0.0.1" "11111"
+cd /root/rtpproxy && erl +K true -noshell -s rtpproxy start "127.0.0.1" "11111"
+
---
+++ rtpsocket.erl
@@ -0,0 +1,22 @@
+-module(rtpsocket).
+
+%-behavior(gen_fsm).
+%-export([start_link/1]).
+%-export([send_event/2]).
+
+-export([watcher/1]).
+
+
+watcher (Parent) ->
+	receive
+		{udp, Fd, Ip, Port, Msg} ->
+			gen_server:cast(Parent, {udp, {Fd, Ip, Port, Msg}}),
+			watcher(Parent);
+		{Parent, destroy} ->
+			ok;
+		_Other ->
+			watcher(Parent)
+	after 3000 ->
+		gen_server:cast(Parent, {self(), timeout})
+	end.
+
