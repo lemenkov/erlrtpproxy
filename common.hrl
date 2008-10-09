67b32fb8fd3345294306dd7b18506594cbf8fa65
Peter Lemenkov <lemenkov@gmail.com>
2008-10-09 09:49:29 +0000

    cosmetic

--- common.hrl
+++ common.hrl
@@ -18,18 +18,18 @@
 
 -define (MOD_LIST, [{mod_asymmetric, $A}, {mod_e, $E}, {mod_i, $I}, {mod_ipv6, $6}, {mod_symmetric, $S}, {mod_weak, $W}, {mod_z, $Z}]).
 
--define(CMD_V, 0).
--define(CMD_VF,1).
--define(CMD_U, 2).
--define(CMD_L, 3).
--define(CMD_D, 5).
--define(CMD_R, 6).
--define(CMD_P, 7).
--define(CMD_S, 8).
--define(CMD_C, 9).
--define(CMD_Q, 10).
--define(CMD_X, 11).
--define(CMD_I, 12).
+-define(CMD_V, cmd_v).
+-define(CMD_VF,cmd_vf).
+-define(CMD_U, cmd_u).
+-define(CMD_L, cmd_l).
+-define(CMD_D, cmd_d).
+-define(CMD_R, cmd_r).
+-define(CMD_P, cmd_p).
+-define(CMD_S, cmd_s).
+-define(CMD_C, cmd_c).
+-define(CMD_Q, cmd_q).
+-define(CMD_X, cmd_x).
+-define(CMD_I, cmd_i).
 
 -define(PRINT(X, Y), utils:print(?MODULE, "[~w]: " ++  X ++ "~n", [self()] ++ Y)).
 
