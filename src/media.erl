b54e92fa7c5eea11faf96d590dc6ca7578e8d951
Peter Lemenkov <lemenkov@gmail.com>
2009-04-01 08:18:31 +0000

    temporarily disable RTCP BYE handling

--- src/media.erl
+++ src/media.erl
@@ -147,14 +147,18 @@ handle_info({udp, Fd, Ip, Port, Msg}, State) when Fd == (State#state.fromrtcp)#m
 		Fd == (State#state.fromrtcp)#media.fd ->
 			SafeSendRtcp((State#state.to)#media.fd, (State#state.fromrtcp)#media.ip, (State#state.fromrtcp)#media.port, Msg),
 			case lists:keymember(bye, 1, Rtcps) of
-				true -> {stop, rtcp_bye, State};
+				true ->
+					?ERR("We SHOULD terminate this call due to RTCP BYE", []);
+%					{stop, rtcp_bye, State};
 				_ -> {noreply, State#state{tortcp=(State#state.tortcp)#media{ip=Ip,port=Port,rtpstate=rtp,lastseen=now()}}}
 			end;
 %			{noreply, State#state{tortcp=(State#state.tortcp)#media{ip=Ip,port=Port,rtpstate=rtp,lastseen=now()}}};
 		Fd == (State#state.tortcp)#media.fd ->
 			SafeSendRtcp((State#state.from)#media.fd, (State#state.tortcp)#media.ip, (State#state.tortcp)#media.port, Msg),
 			case lists:keymember(bye, 1, Rtcps) of
-				true -> {stop, rtcp_bye, State};
+				true ->
+					?ERR("We SHOULD terminate this call due to RTCP BYE", []);
+%					{stop, rtcp_bye, State};
 				_ -> {noreply, State#state{fromrtcp=(State#state.fromrtcp)#media{ip=Ip,port=Port,rtpstate=rtp,lastseen=now()}}}
 			end
 %			{noreply, State#state{fromrtcp=(State#state.fromrtcp)#media{ip=Ip,port=Port,rtpstate=rtp,lastseen=now()}}}
