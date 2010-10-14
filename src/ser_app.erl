e931346033c6f72dd767565824132a99aac9d399
Peter Lemenkov <lemenkov@gmail.com>
2010-10-14 22:26:16 +0400

    Ignore StartType
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/ser_app.erl
+++ src/ser_app.erl
@@ -2,8 +2,7 @@
 -behaviour(application).
 -export([start/2, stop/1]).
 
-start(normal, StartArgs) ->
-	application:start(erlsyslog),
+start(_Type, StartArgs) ->
 	supervisor:start_link({local, ser_sup}, ser_sup, StartArgs).
 
 stop(_State) ->
