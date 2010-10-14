4e500b589a2ba7f77c0172eaccf48d9cd81c9ed6
Peter Lemenkov <lemenkov@gmail.com>
2010-10-14 22:24:27 +0400

    Fixed startup
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/ser_sup.erl
+++ src/ser_sup.erl
@@ -4,6 +4,6 @@
 -export([init/1]).
 
 init(Args) ->
-	Child = {ser,{ser,start_link,Args},permanent,2000,worker,[ser]},
+	Child = {ser,{ser,start_link,[Args]},permanent,2000,worker,[ser]},
 	{ok,{{one_for_one,10,1}, [Child]}}.
 
