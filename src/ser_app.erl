93bd3f0ef5c5a9880e0f673e1f95e01d80f535da
Peter Lemenkov <lemenkov@gmail.com>
2010-10-14 17:31:36 +0400

    Fix exporting functions to conform OTP
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/ser_app.erl
+++ src/ser_app.erl
@@ -1,10 +1,13 @@
 -module(ser_app).
 -behaviour(application).
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
-start(normal, _StartArgs) ->
+start() ->
+	start(normal, []).
+
+start(normal, StartArgs) ->
 	application:start(erlsyslog),
-	ser_sup:start_link().
+	supervisor:start_link({local, ser_sup}, ser_sup, StartArgs).
 
 stop(_State) ->
 	ok.
