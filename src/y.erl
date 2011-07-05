164771b9d20fad66464f1736410bbc609a6bba91
Peter Lemenkov <lemenkov@gmail.com>
2011-07-05 09:58:18 +0400

    Added simple test for Y-combinator
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/y.erl
+++ src/y.erl
@@ -1,31 +1,31 @@
-% Y-combinator in Erlang.
-%
-% url: http://en.wikipedia.org/wiki/Fixed-point_combinator
-% license: public domain
-% version: 1.0
+%% Y-combinator in Erlang.
+%%
+%% url: http://en.wikipedia.org/wiki/Fixed-point_combinator
+%% license: public domain
+%% version: 1.0
 
 -module(y).
 -export([y/1]).
--export([test/0]).
--export([test/1]).
 
 y(F) -> G = fun (M) -> F(fun(A) -> (M(M))(A) end) end, G(G).
 
-test(Data) when is_list(Data)->
-	io:format("This test function will reverse list ~p and returns the result.~n", [Data]),
-	TestFun = fun(F) ->
+%%
+%% Tests
+%%
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+y_combinator_test() ->
+	SumFun = fun(F) ->
 			fun	({[], Acc}) ->
 					Acc;
 				({[Head|Tail], Acc}) ->
-					F({Tail, [Head] ++ Acc});
+					F({Tail, Head + Acc});
 				(Array) ->
-					F({Array, []})
+					F({Array, 0})
 			end
 	end,
-	(y:y(TestFun))(Data);
+	?assertEqual(6, (y:y(SumFun))([1,2,3])).
 
-test(_Other) ->
-	ok.
-
-test() ->
-	ok.
+-endif.
