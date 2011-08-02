33778058490a5583cebcb517a6996e1a1b078614
Peter Lemenkov <lemenkov@gmail.com>
2011-08-02 18:00:19 +0400

    Drop outdated stuff
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtpproxy_ctl.erl
+++ src/rtpproxy_ctl.erl
@@ -20,42 +20,9 @@
 -module(rtpproxy_ctl).
 -author('lemenkov@gmail.com').
 
--export([upgrade/0]).
--export([upgrade/1]).
 -export([start/0]).
 -export([stop/0]).
 -export([status/0]).
-
-upgrade() ->
-	Status = case init:get_plain_arguments() of
-		[NodeStr] ->
-			Node = list_to_atom(NodeStr),
-			try rpc:call(Node, gen_server, cast, [{global, rtpproxy}, upgrade], 5000) of
-				{badrpc, Reason} ->
-					2;
-				_ ->
-					0
-			catch _:_ ->
-				2
-			end;
-		_ ->
-			1
-	end,
-	halt(Status).
-
-upgrade(all) ->
-	upgrade(pool:get_nodes());
-
-upgrade(Nodes) when is_list(Nodes) ->
-	lists:foreach(fun upgrade/1, Nodes);
-
-upgrade(Node) when is_atom(Node) ->
-	{ok, Sources} = application:get_env(rtpproxy, sources),
-	lists:foreach(fun(Mod) ->
-			{Mod, Bin, File} = code:get_object_code(Mod),
-			rpc:call(Node, code, load_binary, [Mod, File, Bin])
-		end,
-	Sources).
 
 start() ->
 	% Start our pool
