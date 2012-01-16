26392d1d5510e1ba24f8d7513be4a33a0d3d4d09
Peter Lemenkov <lemenkov@gmail.com>
2012-01-16 18:18:30 +0400

    Sttart frontend at the main node
    No longer split of main and auxiliary nodes.
    
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtpproxy_sup.erl
+++ src/rtpproxy_sup.erl
@@ -13,7 +13,27 @@ init([]) ->
 	MaxTimeBetweenRestarts = 1,
 	SupFlags = {RestartStrategy, MaxRestarts, MaxTimeBetweenRestarts},
 
+	% Load listener
+	{ok, {Proto, IpStr, Port}} = application:get_env(ser, listen),
+	{ok, Ip} = inet_parse:address(IpStr),
+	ListenerProcess = case Proto of
+		tcp ->
+			{tcp_listener, {tcp_listener, start_link, [[Ip, Port]]}, permanent, 10000, worker, []};
+		udp ->
+			{udp_listener, {udp_listener, start_link, [[Ip, Port]]}, permanent, 10000, worker, []}
+	end,
+
+	% Load backend
+	{ok, {Type, Addr}} = application:get_env(ser, backend),
+	BackendProcess = case Type of
+		erlang ->
+			{erlang_backend, {erlang_backend, start_link, [Addr]}, permanent, 10000, worker, []};
+		proxy ->
+			{proxy_backend, {proxy_backend, start_link, [Addr]}, permanent, 10000, worker, []}
+	end,
+
+	% Load main module
 	RtpProxy = {rtpproxy,{rtpproxy,start_link,[ignored]},permanent,2000,worker,[rtpproxy]},
 
-	{ok,{SupFlags, [RtpProxy]}}.
+	{ok,{SupFlags, [ListenerProcess, BackendProcess, RtpProxy]}}.
 
