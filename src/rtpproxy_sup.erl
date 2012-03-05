33726c274e9eac3e126c00b78c442e96b11ff583
Peter Lemenkov <lemenkov@gmail.com>
2012-03-05 14:10:38 +0400

    Load frontend and backend for SER interface
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtpproxy_sup.erl
+++ src/rtpproxy_sup.erl
@@ -13,7 +13,7 @@ init([]) ->
 	MaxTimeBetweenRestarts = 1,
 	SupFlags = {RestartStrategy, MaxRestarts, MaxTimeBetweenRestarts},
 
-	% Load listener
+	% Load SER listener
 	application:load(ser),
 	{ok, {Proto, IpStr, Port}} = application:get_env(ser, listen),
 	{ok, Ip} = inet_parse:address(IpStr),
@@ -24,8 +24,12 @@ init([]) ->
 			{udp_listener, {udp_listener, start_link, [[Ip, Port]]}, permanent, 10000, worker, []}
 	end,
 
+	% Load SER backend
+	{ok, Addr} = application:get_env(ser, backend),
+	BackendProcess = {backend, {backend, start_link, [Addr]}, permanent, 10000, worker, []},
+
 	% Load main module
 	RtpProxy = {rtpproxy,{rtpproxy,start_link,[ignored]},permanent,2000,worker,[rtpproxy]},
 
-	{ok,{SupFlags, [ListenerProcess, RtpProxy]}}.
+	{ok,{SupFlags, [ListenerProcess, BackendProcess, RtpProxy]}}.
 
