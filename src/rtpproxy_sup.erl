51f68f7c706619d10fd6908f2edab0bd499c64b1
Peter Lemenkov <lemenkov@gmail.com>
2012-05-30 23:38:41 +0400

    Fix comments
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtpproxy_sup.erl
+++ src/rtpproxy_sup.erl
@@ -13,7 +13,7 @@ init([]) ->
 	MaxTimeBetweenRestarts = 1,
 	SupFlags = {RestartStrategy, MaxRestarts, MaxTimeBetweenRestarts},
 
-	% Load SER listener
+	% Load generic erlrtpproxy controlling interface listener
 	application:load(ser),
 	{ok, {Proto, IpStr, Port}} = application:get_env(ser, listen),
 	{ok, Ip} = inet_parse:address(IpStr),
@@ -24,7 +24,7 @@ init([]) ->
 			{udp_listener, {udp_listener, start_link, [[Ip, Port]]}, permanent, 10000, worker, []}
 	end,
 
-	% Load SER backend
+	% Load protocol backend (ser for now)
 	{ok, Addr} = application:get_env(ser, backend),
 	BackendProcess = {backend, {backend, start_link, [Addr]}, permanent, 10000, worker, []},
 
