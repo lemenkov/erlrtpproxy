e7bf91b1f9c78ff55359472e820550214d4f6a40
Peter Lemenkov <lemenkov@gmail.com>
2012-05-31 16:10:01 +0400

    Remove obsolete single-point-of-failure rtpproxy.erl
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtpproxy_ctl.erl
+++ src/rtpproxy_ctl.erl
@@ -23,6 +23,10 @@
 -export([start/0]).
 -export([stop/0]).
 -export([status_pp/0]).
+-export([status/0]).
+-export([command/1]).
+
+-include("../include/common.hrl").
 
 start() ->
 	% Start our pool
@@ -79,7 +83,7 @@ status_pp() ->
 				{badrpc, _} ->
 					4;
 				{ok, rtpproxy} ->
-					io:format("~s", [rtpproxy:status()]),
+					io:format("~s", [status()]),
 					0;
 				undefined ->
 					ok = io:format("~n"),
@@ -93,4 +97,61 @@ status_pp() ->
 			4
 	end,
 	halt(Status).
+
+% FIXME this must be reworked (no preformatted strings - just plain stats)
+status() ->
+	Calls = ets:match(gproc, {{'$1', {'_', '_', {id, '$2', '$3'}}},'_'}),
+	Header = io_lib:format("Current state - ~p media stream(s):~n", [length(Calls)]),
+	?INFO(Header, []),
+	MediaInfos = lists:map(fun({Pid, CallId, MediaId}) ->
+			{ok, Reply} = gen_server:call(Pid, ?CMD_Q),
+			MediaInfo = io_lib:format("* CallID: ~s, MediaId: ~p, ~s~n", [CallId, MediaId, Reply]),
+			?INFO(MediaInfo, []),
+			MediaInfo
+		end,
+	Calls),
+	lists:flatten([Header] ++ MediaInfos).
+
+% Simply stop all active sessions
+command(#cmd{type = ?CMD_X}) ->
+	lists:foreach(fun(Pid) -> gen_server:cast(Pid, stop) end, ets:match(gproc, {{'$1', {'_', '_', {id, '_', '_'}}},'_'}));
+
+command(#cmd{type = ?CMD_I, params = [brief]}) ->
+	Length = length(ets:match(gproc, {{'$1', {'_', '_', {id, '_', '_'}}},'_'})),
+	{ok, {stats, Length}};
+
+% TODO show information about calls
+command(#cmd{type = ?CMD_I}) ->
+	% Calls = ets:match(gproc, {{'$1', {'_', '_', {id, '_', '_'}}},'_'}),
+	% Stats = lists:map(fun(Pid) -> gen_server:call(Pid, ?CMD_Q) end, Calls),
+	% "sessions created: %llu\nactive sessions: %d\n active streams: %d\n"
+	% foreach session "%s/%s: caller = %s:%d/%s, callee = %s:%d/%s, stats = %lu/%lu/%lu/%lu, ttl = %d/%d\n"
+	Length = length(ets:match(gproc, {{'$1', {'_', '_', {id, '_', '_'}}},'_'})),
+	{ok, {stats, Length}};
+
+command(#cmd{callid = CallId, mediaid = MediaId} = Cmd) ->
+	% First try to find existing session(s)
+	case get_media(CallId, MediaId) of
+		[] when Cmd#cmd.type == ?CMD_U ->
+			% Create new media thread
+			?INFO("Media stream does not exist. Creating new.", []),
+			pool:pspawn(media, start, [Cmd]);
+		[] ->
+			?WARN("Media stream does not exist. Do nothing.", []),
+			{error, notfound};
+		MediaThreads when is_list(MediaThreads) ->
+			lists:foreach(fun(Pid) -> gen_server:cast(Pid, Cmd) end, MediaThreads);
+		MediaThread ->
+			% Operate on existing media thread
+			gen_server:cast(MediaThread, Cmd)
+	end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% Internal functions %%
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+get_media(CallId, 0) ->
+	ets:match(gproc, {{'$1', {'_', '_', {id, CallId, '_'}}},'_'});
+get_media(CallId, MediaId) ->
+	ets:match(gproc, {{'$1', {'_', '_', {id, CallId, MediaId}}},'_'}).
 
