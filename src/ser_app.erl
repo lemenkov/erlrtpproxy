57e896e59f5559dcc40519b6a9aa53e427483920
Peter Lemenkov <lemenkov@gmail.com>
2012-07-22 13:04:01 +0400

    Remove unused ser-related stuff
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/ser_app.erl
+++
@@ -1,9 +0,0 @@
--module(ser_app).
--behaviour(application).
--export([start/2, stop/1]).
-
-start(_Type, StartArgs) ->
-	supervisor:start_link({local, ser_sup}, ser_sup, StartArgs).
-
-stop(_State) ->
-	ok.
