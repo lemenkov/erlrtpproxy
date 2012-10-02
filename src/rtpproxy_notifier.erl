5c0e4afae27d8aa8db8e349ee4ce01856f325cc3
Peter Lemenkov <lemenkov@gmail.com>
2012-10-02 18:47:24 +0400

    Revert "Ver. 0.10.4"
    Partiall reverts some erroneously committed stuff
    
    This reverts commit bfe695839ac9d76a617d8f729cf69be7633468a5.

--- src/rtpproxy_notifier.erl
+++ src/rtpproxy_notifier.erl
@@ -16,7 +16,6 @@ start_link() ->
 	gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
 init(_) ->
-	error_logger:info_msg("NOT: ~p ~p~n", [application:get_env(rtpproxy, radacct_servers), application:get_env(rtpproxy, notify_servers)]),
 	RadiusBackend = case application:get_env(rtpproxy, radacct_servers) of
 		{ok, RadAcctServers} ->
 			rtpproxy_notifier_backend_sup:start_link(radius, RadAcctServers),
@@ -33,7 +32,7 @@ init(_) ->
 	end,
 	{ok, IgnoreStart} = application:get_env(rtpproxy, ignore_start),
 	{ok, IgnoreStop} = application:get_env(rtpproxy, ignore_stop),
-	error_logger:info_msg("Started Notifier at ~p [~p ~p]~n", [node(), IgnoreStart, IgnoreStop]),
+	error_logger:info_msg("Started Notifier at ~p~n", [node()]),
 	{ok, #state{
 			radius = RadiusBackend,
 			notify = NotifyBackend,
@@ -50,7 +49,6 @@ handle_cast({Type, CallId, MediaId, Addr}, #state{radius = RadiusBackend, notify
 	Type == start;
 	Type == interim_update;
 	Type == stop ->
-	error_logger:warning_msg("CAST: ~p~n", [State]),
 	Send = ((Type == start) and not IgnoreStart) or (Type == interim_update) or ((Type == stop) and not IgnoreStop),
 	(RadiusBackend and Send) andalso gen_server:cast(rtpproxy_notifier_backend_radius, {Type, CallId, MediaId, Addr}),
 	(NotifyBackend and Send) andalso gen_server:cast(rtpproxy_notifier_backend_notify, {Type, CallId, MediaId, Addr}),
