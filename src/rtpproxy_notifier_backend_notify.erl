3d0cbad0b0e0c420c05ff0ed50e8bbe6d78ebc9b
Peter Lemenkov <lemenkov@gmail.com>
2012-11-30 16:27:28 +0400

    Prepare for TCP notifications
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/rtpproxy_notifier_backend_notify.erl
+++ src/rtpproxy_notifier_backend_notify.erl
@@ -13,20 +13,26 @@
 start_link(Args) ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
-% FIXME add more types than udp
+init(tcp) ->
+	error_logger:info_msg("Started rtpproxy notify protocol backend (TCP) at ~p~n", [node()]),
+	{ok, {tcp, []}};
 init(udp) ->
 	{ok, Fd} = gen_udp:open(0, [binary, {active, true}]),
 	error_logger:info_msg("Started rtpproxy notify protocol backend (UDP) at ~p~n", [node()]),
-	{ok, Fd}.
+	{ok, {udp, Fd}}.
 
 handle_call(Message, From, State) ->
 	error_logger:warning_msg("Bogus call: ~p from ~p at ~p~n", [Message, From, node()]),
 	{reply, {error, unknown_call}, State}.
 
-handle_cast({Type, _, _, [{addr,{Ip,Port}},{tag,NotifyTag}]}, Fd) ->
+handle_cast({Type, _, _, [{addr,{Ip,Port}},{tag,NotifyTag}]}, {tcp, FdSet}) ->
+	% FIXME
+	error_logger:info_msg("Message (~p) delivered from ~p to ~s:~b~n", [Type, node(), inet_parse:ntoa(Ip), Port]),
+	{noreply, {tcp, FdSet}};
+handle_cast({Type, _, _, [{addr,{Ip,Port}},{tag,NotifyTag}]}, {udp, Fd}) ->
 	gen_udp:send(Fd, Ip, Port, NotifyTag),
 	error_logger:info_msg("Message (~p) delivered from ~p to ~s:~b~n", [Type, node(), inet_parse:ntoa(Ip), Port]),
-	{noreply, Fd};
+	{noreply, {udp, Fd}};
 
 handle_cast(stop, State) ->
 	{stop, normal, State};
@@ -42,7 +48,11 @@ handle_info(Other, State) ->
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
 
-terminate(Reason, Fd) ->
+terminate(Reason, {tcp, FdSet}) ->
+	lists:map(fun(X) -> gen_tcp:close(X) end, FdSet),
+	error_logger:error_msg("Terminated: ~p at ~p~n", [Reason, node()]),
+	ok;
+terminate(Reason, {udp, Fd}) ->
 	gen_udp:close(Fd),
 	error_logger:error_msg("Terminated: ~p at ~p~n", [Reason, node()]),
 	ok.
