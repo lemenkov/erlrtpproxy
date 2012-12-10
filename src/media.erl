c9dd54029181dbb042fef1e8c9090dcd8c8209a8
Peter Lemenkov <lemenkov@gmail.com>
2012-12-10 20:39:48 +0400

    Quick fix for timeout
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/media.erl
+++ src/media.erl
@@ -65,7 +65,7 @@ init([#cmd{type = ?CMD_U, callid = C, mediaid = M, from = #party{tag = T}, param
 	end,
 
 	{ok, RebuildRtp} = application:get_env(rtpproxy, rebuildrtp),
-	{ok, Pid} = gen_rtp_channel:open(0, Params ++ [{ip, I}, {rebuildrtp, RebuildRtp}]),
+	{ok, Pid} = gen_rtp_channel:open(0, Params ++ [{ip, I}, {rebuildrtp, RebuildRtp}, {timeout, 75000}]),
 
 	NotifyInfo = proplists:get_value(notify, Params, []),
 
