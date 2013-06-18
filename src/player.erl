0b15b9e9b3f435e2755de5405d88ecbbf29a373e
Peter Lemenkov <lemenkov@gmail.com>
2013-06-18 16:49:52 +0400

    Use gen_server:info instead of cast
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/player.erl
+++ src/player.erl
@@ -99,7 +99,7 @@ handle_info(send, #state{subscriber = Subscriber, marker = Marker, sn = Sequence
 			extension = null,
 			payload = Payload
 		},
-	gen_server:cast(Subscriber, {rtp:encode(Pkt), null, null}),
+	Subscriber ! {rtp:encode(Pkt), null, null},
 	{noreply, State#state{marker = 0, sn = SequenceNumber + 1}};
 
 handle_info(Info, State) ->
