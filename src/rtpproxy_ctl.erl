59e2ec1a8651188403aace7757e764ff4ee19f35
Peter Lemenkov <lemenkov@gmail.com>
2013-06-21 18:16:29 +0400

    Revert "Start gproc again"
    This reverts commit d274f24afc0397bac3db8f0e0571ec39d858cee8.

--- src/rtpproxy_ctl.erl
+++ src/rtpproxy_ctl.erl
@@ -32,9 +32,6 @@ start() ->
 	error_logger:tty(false),
 	error_logger:add_report_handler(erlsyslog),
 
-	%% Run gproc
-	application:start(gproc),
-
 	%% Load necessary config files
 	application:load(rtpproxy),
 
